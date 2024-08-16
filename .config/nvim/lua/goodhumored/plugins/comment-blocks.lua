82b679e45f6410b5c6a7f89967ac2c3ce2f52363
goodhumored <goodhumored@vk.com>
2024-08-16 11:08:22 +0300

    nvim plugins
    - comment blocks keymaps updated to gc+...
    - nvim spider plugin
    - git config plugin
    - conflict marker plugin
    - tint plugin
    - visual multi plugin

--- .config/nvim/lua/goodhumored/plugins/comment-blocks.lua
+++ .config/nvim/lua/goodhumored/plugins/comment-blocks.lua
@@ -1,8 +1,8 @@
 return {
 	"LudoPinelli/comment-box.nvim",
 	config = function()
-		vim.keymap.set({ "n", "v" }, "<leader>cb", "<cmd>CBccbox<CR>", { desc = "[C]omment [B]lock" })
-		vim.keymap.set({ "n", "v" }, "<leader>cl", "<cmd>CBccline<CR>", { desc = "[C]omment [L]ine" })
-		vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CBline<CR>", { desc = "[C]omment Simple [L]ine" })
+		vim.keymap.set({ "n", "v" }, "gcb", "<cmd>CBccbox<CR>", { desc = "[C]omment [B]lock" })
+		vim.keymap.set({ "n", "v" }, "gcl", "<cmd>CBccline<CR>", { desc = "[C]omment [L]ine" })
+		vim.keymap.set({ "n", "v" }, "gc-", "<cmd>CBline<CR>", { desc = "[C]omment Simple [L]ine" })
 	end,
 }
---
+++ .config/nvim/lua/goodhumored/plugins/git-conflict.lua
@@ -0,0 +1,5 @@
+--          ╭─────────────────────────────────────────────────────────╮
+--          │                      git-conflict                       │
+--          │  A plugin to visualise and resolve conflicts in neovim  │
+--          ╰─────────────────────────────────────────────────────────╯
+return { "akinsho/git-conflict.nvim", version = "*", config = true }
