711a513410b50dc6009985d796226017df853820
goodhumored <goodhumored@vk.com>
2024-08-16 11:02:20 +0300

    timg preview for videos added

--- .config/scripts/preview-file.sh
+++ .config/scripts/preview-file.sh
@@ -7,7 +7,7 @@ filename=$(basename -- "$1")
 extension="${filename##*.}"
 
 case "$extension" in
-  png|jpg|webp) echo "$(timg -p quarter -g50x50 $1)" ;;
+  png|jpg|webp|mp4|mkv) echo "$(timg -p quarter -g50x50 $1)" ;;
   7z|zip|gz) echo "$(7z l $1)" ;;
   mp3|wav) echo "playing..." && paplay $1 ;;
   *) echo "$(bat -n --color=always --line-range :500 $1)" ;;
