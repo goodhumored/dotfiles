fd4ae0720b0e8cf095c76459ff4883cff0d2ec8b
goodhumored <goodhumored@vk.com>
2024-08-16 11:08:22 +0300

    nvim plugins
    - comment blocks keymaps updated to gc+...
    - nvim spider plugin
    - git config plugin
    - conflict marker plugin
    - tint plugin
    - visual multi plugin

---
+++ .config/nvim/lua/goodhumored/plugins/spider.lua
@@ -0,0 +1,26 @@
+--          ╭─────────────────────────────────────────────────────────╮
+--          │                chrisgrieser/nvim-spider                 │
+--          │                      ------------                       │
+--          │    Replaces word jump mappings to improve horizontal    │
+--          │                       navigation                        │
+--          ╰─────────────────────────────────────────────────────────╯
+return {
+	"chrisgrieser/nvim-spider",
+	keys = {
+		{
+			"e",
+			"<cmd>lua require('spider').motion('e')<CR>",
+			mode = { "n", "o", "x" },
+		},
+		{
+			"w",
+			"<cmd>lua require('spider').motion('w')<CR>",
+			mode = { "n", "o", "x" },
+		},
+		{
+			"b",
+			"<cmd>lua require('spider').motion('b')<CR>",
+			mode = { "n", "o", "x" },
+		},
+	},
+}
