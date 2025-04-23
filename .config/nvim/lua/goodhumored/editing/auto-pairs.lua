8cd0b5a1db58436a411f92f20b54ebb7258fd0b6
goodhumored <goodhumored@vk.com>
2025-04-23 15:40:58 +0300

    fix(nvim): some shitty error with auto-pairs and neotree

--- .config/nvim/lua/goodhumored/editing/auto-pairs.lua
+++ .config/nvim/lua/goodhumored/editing/auto-pairs.lua
@@ -1,3 +1,18 @@
 return {
-	"jiangmiao/auto-pairs",
+	"windwp/nvim-autopairs",
+	-- config = function()
+	-- 	require("nvim-autopairs").setup({
+	-- 		check_ts = true, -- Опционально, если используешь treesitter
+	-- 	})
+	--
+	-- 	-- Переопределяем автокомманду BufEnter
+	-- 	vim.api.nvim_create_autocmd("BufEnter", {
+	-- 		group = vim.api.nvim_create_augroup("NvimAutopairsCustom", { clear = true }),
+	-- 		callback = function()
+	-- 			if vim.bo.filetype ~= "NvimTree" then
+	-- 				require("nvim-autopairs").on_enter()
+	-- 			end
+	-- 		end,
+	-- 	})
+	-- end,
 }
