cc586564bf1ef492a2e31372e6279d94bcaa0b4d
goodhumored <goodhumored@vk.com>
2025-04-23 15:35:09 +0300

    feat: crow scripts

--- .config/hypr/scripts/translate-clipboard.sh
+++ .config/hypr/scripts/translate-clipboard.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Получаем выделенный текст (primary selection)
-selected_text=$(wl-paste --primary)
+copied_text=$(wl-paste)
 
 # Проверяем, есть ли выделенный текст
-if [ -z "$selected_text" ]; then
+if [ -z "$copied_text" ]; then
   notify-send "Ошибка" "Нет выделенного текста"
   exit 1
 fi
 
 # Переводим текст с помощью crow (на русский, например)
-translated_text=$(echo "$selected_text" | crow -i -t ru)
+translated_text=$(echo "$copied_text" | crow -i -t ru)
 
 # Выводим результат через notify-send
 notify-send "Перевод" "$translated_text"
